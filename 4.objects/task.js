d0ccfd38ca96411d6749fbc489e575fb6ab046a6
Ezhik-Mari <137823899+Ezhik-Mari@users.noreply.github.com>
2023-08-05 19:28:32 +0300

    Add files via upload

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.setSubject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if('marks' in this) {
+    this.marks.push(...marks);
+   }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.marks === undefined || this.marks.length < 1) {
+    return 0;
+  }
+  else {
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+    }
+  return sum / this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
