c112bc0a85498dd6e2145ccbc12e3b8c74b8af85
Ezhik-Mari <137823899+Ezhik-Mari@users.noreply.github.com>
2023-08-05 19:29:20 +0300

    Add files via upload

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,46 @@
-﻿
+﻿function parseCount(products) {
+    let resultParse = Number.parseFloat(products);
+    if (Number.isNaN(resultParse)) {
+        throw new Error('Невалидное значение');
+    }
+    return resultParse;
+}
+function validateCount(products) {
+    try {
+        return parseCount(products);
+    } catch(error) {
+        return error;
+    }
+}
+class Triangle {
+    consructor(sideOne, sideTwo, sideThree) {
+        if ((sideOne + sideTwo) < sideThree || (sideTwo + sideThree) <sideOne || (sideOne + sideThree) < sideTwo) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.sideOne = sideOne;
+        this.sideTwo = sideTwo;
+        this.sideThree = sideThree;
+    }
+    get perimeter() {
+        return + (this.sideOne + this.sideTwo + this.sideThree);
+    }
+    get area() {
+        let p = this.perimeter / 2;
+        return Number (Math.sqrt(p * (p - this.sideOne) * (p - this.sideTwo) * (p - this.sideThree)).toFixed(3));
+    }
+}
+function getTriangle(sideOne, sideTwo, sideThree) {
+    try {
+        return new Triangle(sideOne, sideTwo, sideThree);
+    }
+    catch(error) {
+        return {
+            get Perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get Area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
