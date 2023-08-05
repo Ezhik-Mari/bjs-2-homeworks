6143e5517b9ae6fea419978920925f5e950e73bc
Ezhik-Mari <137823899+Ezhik-Mari@users.noreply.github.com>
2023-08-05 19:27:49 +0300

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+  let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+  return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
+    return resultArr;
 }
